34ffa0aaf73939de28b2a85351dfc9080843b10f
yedyaditya <yedyaditya@gmail.com>
2020-06-04 14:22:41 +0700

    fixing add new donation user

--- admin/donation_add.php
+++ admin/donation_add.php
@@ -6,7 +6,7 @@
         $email = $_POST['email'];
         $nominal = $_POST['nominal'];
 
-        // $pdo = new Database();
+        $pdo = new Database();
         $conn = $pdo->open();
 
         $now = date('Y-m-d');
@@ -17,16 +17,16 @@
         $lastId = $conn->lastInsertId();
 
         try{
-            // Ambil data dari detail_transaction join dengan users dan transaction table
-            $stmt = $conn->prepare("SELECT*, COUNT(*) AS numrows FROM detail_transaction as d JOIN users as u on u.id=d.id_user JOIN transaction as t ON t.id = d.id_transaction WHERE email=:email");
-            $stmt->execute(['email' => $email]);
-            $row = $stmt->fetch();
+            // Ambil data dari table users
+            // $stmt = $conn->prepare("SELECT*, COUNT(*) AS numrows FROM detail_transaction as d JOIN users as u on u.id=d.id_user JOIN transaction as t ON t.id = d.id_transaction WHERE email=:email");
+            // $stmt->execute(['email' => $email]);
+            // $row = $stmt->fetch();
 
             // Generate kode transaksi
-            $code_trx = $row['code_trx'];
-            $code = 'n100';
+            $code_trx = 'TRX';
+            $code = 100;
             $date = date("F", strtotime($now));
-            $generate_code = $code_trx .'-'. $code++ .'-'. $date;
+            $generate_code = $code_trx .'-N'. $code++ .'-'. $date;
 
             // Status trx default 0
             $status_trx = 0;
@@ -36,8 +36,12 @@
                 $_SESSION['error'] = 'Maaf, Transaksi sudah ada';
             }
             else{
-                // Cek lagi jika email pada detail_transaksi != dengan yang di input
-                if ($row['email'] != $email) {
+                $stmt = $conn->prepare("SELECT * FROM users WHERE email=:email");
+                $stmt->execute(['email' =>$email]);
+                $user = $stmt->fetch();
+                // Cek jika status user belum activated
+                if ($user['status'] != 1 ) {
+                    // Hapus id transaction di sini
                     $stmt = $conn->prepare("DELETE FROM transaction WHERE id=:id");
                     $stmt->execute(['id' => $lastId]);   
                     $_SESSION['error'] = 'Maaf, User belum terdaftar';
@@ -45,10 +49,13 @@
                 else{
                     try {
                         $stmt = $conn->prepare("INSERT INTO detail_transaction (id_transaction, id_user, code_trx, status_trx) VALUES (:id_transaction, :id_user, :code_trx, :status_trx)");
-                        $stmt->execute(['id_transaction'=>$lastId, 'id_user'=>$row['id_user'], 'code_trx'=>$generate_code, 'status_trx'=>$status_trx]);
+                        $stmt->execute(['id_transaction'=>$lastId, 'id_user'=>$user['id'], 'code_trx'=>$generate_code, 'status_trx'=>$status_trx]);
                         $_SESSION['success'] = 'Transaksi Berhasil';
                     }
                     catch(PDOException $e) {
+                        // Hapus id transaction juga di sini
+                        $stmt = $conn->prepare("DELETE FROM transaction WHERE id=:id");
+                        $stmt->execute(['id' => $lastId]);   
                         $_SESSION['error'] = 'Maaf, Transaksi gagal! User belum terdaftar'.$e->getMessage();
                     }
                 }                
